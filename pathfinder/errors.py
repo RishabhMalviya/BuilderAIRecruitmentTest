2110d114d114e6966a6908c138b06e90c4d74472
Rishabh Malviya <rishabh.malviya@gmail.com>
2020-05-31 20:21:26 +0530

    remove invalid input error based on re-read of problem statement

--- pathfinder/errors.py
+++ pathfinder/errors.py
@@ -2,9 +2,5 @@ class BaseError(Exception):
     """Base package error"""
 
 
-class InvalidInputFormatError(BaseError):
-    """Represents an invalid input from input file describing treasure map"""
-
-
 class ShortestPathNotFoundError(BaseError):
     """Represents that no valid shortest path exists from start point to treasure"""
